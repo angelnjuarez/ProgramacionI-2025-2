ae517ab55c6971a1f6bf218d87e013491b9b83c8
unknown <angelnjuarez@gmail.com>
2025-09-08 11:40:00 -0300

    IREP

--- src/Clase06Objetos/Fecha.java
+++ src/Clase06Objetos/Fecha.java
@@ -2,20 +2,35 @@ package Clase06Objetos;
 
 public class Fecha {
 	// variables de instancia
-	int dia;
-	int mes;
-	int anio;
+	private int dia;
+	private int mes;
+	private int anio;
 
+	/*
+	 * Regla de negocio: decidimos que los años son mayores al 2000
+	 * Pre-Condicion: 
+	 */
+	
+	/*
+	 * IREP:
+	 * -	this.anio >= 2000
+	 * -	1 <= this.mes <= 12
+	 * -	El dia tiene que ser mayor o igual que 1
+	 * 		De acuerdo al mes y el año, debe ser menor
+	 * 		o igual a 28, 29, 30 o 31.
+	 */
+	
+	
 	public Fecha(int dia, int mes, int anio) {
-		this.dia = dia;
-		this.mes = mes;
-		this.anio = anio;
+		setAnio(anio);
+		setMes(mes);
+		setDia(dia);
 	}
 
 	public Fecha(int dia, String nombreMes, int anio) {
-		this.dia = dia;
-		this.anio = anio;
-		this.mes = convertir(nombreMes);
+		setAnio(anio);
+		setMes(convertir(nombreMes));
+		setDia(dia);
 	}
 
 	public int convertir(String m) {
@@ -92,4 +107,41 @@ public class Fecha {
 	public int diasDeDiferenciaCon(Fecha otra) {
 		return Math.abs(cantDias(this) - cantDias(otra));
 	}
+
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		int diasDelMes = diasDelMes(this.mes, this.anio);
+		if (dia < 1 || dia > diasDelMes) {
+			throw new RuntimeException("Debe ser un día valido");
+		}
+		this.dia = dia;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		// Pre-condicion
+		if (mes < 1 || mes > 12) {
+			throw new RuntimeException("Debe ser un mes valido");
+		}
+		this.mes = mes;
+	}
+	// Post-condicion: que el this.mes tenga el valor 'mes' si era valido
+
+	public int getAnio() {
+		return anio;
+	}
+
+	public void setAnio(int anio) {
+		if (anio < 2000) {
+			throw new RuntimeException("Año no valido");
+		}
+		this.anio = anio;
+	}	
+
 }
--- src/Clase06Objetos/TestFecha.java
+++ src/Clase06Objetos/TestFecha.java
@@ -3,14 +3,19 @@ package Clase06Objetos;
 public class TestFecha {
 
 	public static void main(String[] args) {
-		Fecha f1=new Fecha(3,9,2025);
-		f1.imprimir();
-//		Fecha f2=new Fecha(20,"Agosto",2024);
+//		Fecha f1 = new Fecha(3, 9, 2025);
+//		f1.imprimir();
+//		Fecha f2 = new Fecha(20, "Agosto", 2024);
 //		f2.imprimir();
-//		System.out.println(Fecha.diasDelMes(6,2025));
-//        System.out.println(f1.esValida());
-		Fecha f3= new Fecha(20,9,2025);
-		System.out.println(f1.antesQue(f3));
-	}
+//		System.out.println(Fecha.diasDelMes(6, 2025));
+//		System.out.println(f1.esValida());
+//		Fecha f3 = new Fecha(20, 9, 2025);
+//		System.out.println(f1.antesQue(f3));
 
+		Fecha f1 = new Fecha(8, 9, 2025);
+		f1.imprimir();
+		Fecha f2 = new Fecha(29, 2, 2025);
+		f2.imprimir();
+
+	}
 }
